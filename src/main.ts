9002e8651b8b861bacfea2abbfb78f78ce7b5e62
Julio Romero <julio.romero@tutenlabs.com>
2024-09-29 15:05:10 -0400

    feat: bff end

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,28 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { AllExceptionFilter } from './common/filters/http-exception-filter';
+import { TimeOutInterceptor } from './common/interceptors/timeout.interceptor';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.enableCors();
+  app.useGlobalFilters(new AllExceptionFilter());
+  app.useGlobalInterceptors(new TimeOutInterceptor());
+  app.useGlobalPipes(new ValidationPipe());
+  const options = new DocumentBuilder()
+      .setTitle('Wallet API')
+      .setDescription('a awesome API')
+      .setVersion('1.0.0')
+      .addBearerAuth()
+      .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('/api/docs', app, document, {
+    swaggerOptions: {
+      filter: true,
+    },
+  });
+  await app.listen(3002);
 }
 bootstrap();
